866ed225ba24181e6e004c11e550d77813134c4b
Ben Roa <benihanas1@gmail.com>
2016-06-07 16:49:33 -0700

    (feat) complete single payment process

---
+++ server/controllers/StripeController.js
@@ -0,0 +1,36 @@
+var User = require('./../models/UserModel.js');
+var bcrypt = require('bcrypt-nodejs');
+var Promise = require('bluebird');
+
+var stripe = require("stripe")("sk_test_PanpveFjGob7BUFMW4zLaQ2l");
+
+exports.chargeCard = function(req, res) {
+  console.log('THECHARGECARDBODY', req.body);
+  console.log('THE USER', req.user);
+  
+  var stripeToken = req.body.stripeToken;
+
+  var charge = stripe.charges.create({
+  amount: 1000, // amount in cents, again
+  currency: "usd",
+  source: stripeToken,
+  description: "Example charge"
+  }, function(err, charge) {
+    if (err && err.type === 'StripeCardError') {
+    // The card has been declined
+      console.log('CARD DECLINED');
+    } else {
+      //post to user that payed
+      console.log('CARD PASS?');
+      User.where({ email: req.user.attributes.email }).fetch().then(function(user) {
+        console.log('THE USER', user);
+        //user.payed = true;
+        user.save({payed: true})
+      });
+    }
+  });
+
+};
+
+
+
